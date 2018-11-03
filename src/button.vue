4c335b82aa232d1afa1abfc735097d30d5fa764e
evenyao <invictus@vip.qq.com>
2018-11-03 14:18:32 +0800

    添加 button 的 icon 和 iconPosition 属性

--- src/button.vue
+++ src/button.vue
@@ -1,14 +1,26 @@
 <template>
-  <button class="gear-button">按钮</button>
+  <button class="gear-button" :class="{[`icon-${iconPosition}`]: true}">
+    <svg v-if="icon" class="icon">
+      <use v-bind:xlink:href="`#i-${icon}`"></use>
+    </svg>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </button>
 </template>
 
 <script>
-  export default {}
+  export default {
+    props: ['icon', 'iconPosition']
+  }
 
 </script>
 
 <style lang="scss">
   .gear-button {
+    display: inline-flex; justify-content: center; align-items: center;
+    vertical-align: middle;
+
     font-size: var(--font-size);
     height: var(--button-height);
     padding: 0 1em;
@@ -16,14 +28,15 @@
     border: 1px solid var(--border-color);
     background: var(--button-bg);
 
-    &:hover {
-      border-color: var(--border-color-hover);
-    }
-    &:active {
-      background-color: var(--button-active-bg);
-    }
-    &:focus {
-      outline: none;
+    &:hover { border-color: var(--border-color-hover); }
+    &:active { background-color: var(--button-active-bg); }
+    &:focus { outline: none; }
+    > .icon { order: 1; margin-right: .2em; }
+    > .content { order: 2; }
+
+    &.icon-right {
+      > .icon { order: 2; margin-right: 0; margin-left: .2em; }
+      > .content { order: 1; }
     }
   }
 </style>
