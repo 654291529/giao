4d4c86a977225c41205899735a1d7ae14c8c108a
evenyao <invictus@vip.qq.com>
2018-11-04 13:42:22 +0800

    创建 button-group

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
+import ButtonGroup from './button-group'
 
 Vue.component('gear-button', Button)
 Vue.component('gear-icon', Icon)
+Vue.component('gear-button-group', ButtonGroup)
 
 new Vue({
   el: '#app',
---
+++ src/button-group.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="gear-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss">
+  .gear-button-group {
+    display: inline-flex;
+    vertical-align: middle;
+    > .gear-button {
+      border-radius: 0;
+      margin-left: -1px;
+      &:first-child {
+        border-top-left-radius: var(--border-radius);
+        border-bottom-left-radius: var(--border-radius);
+      }
+      &:last-child {
+        border-top-right-radius: var(--border-radius);
+        border-bottom-right-radius: var(--border-radius);
+      }
+      /* 解决 margin-left: -1px ，border-left 被前一按钮 border-right 覆盖的问题 */
+      &:hover {
+        position: relative;
+        z-index: 1;
+      }
+    }
+  }
+
+</style>
