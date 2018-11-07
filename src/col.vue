3f916ee494d09591c50a485fcd64c80c0708435f
evenyao <invictus@vip.qq.com>
2018-11-07 11:50:55 +0800

    基本实现 row 和 col 的效果功能

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-  <div class="col" :class="[`col-${span}`]">
-    <slot></slot>
+  <div class="col" :class="[span && `col-${span}`, offset && `offset-${offset}`]"
+    :style="{ paddingLeft: gutter/2 + 'px', paddingRight: gutter/2 + 'px' }">
+    <div style="border: 1px solid green; height: 100px; background: lightblue;">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
@@ -10,6 +13,15 @@
     props: {
       span: {
         type: [Number, String]
+      },
+      offset: {
+        type: [Number, String]
+      }
+    },
+    data() {
+      return {
+        // 接收从 row 传递过来的 gutter
+        gutter: 0
       }
     }
   }
@@ -18,9 +30,10 @@
 <style lang="scss" scoped>
   .col {
     height: 100px;
-    background: lightblue;
+    /*background: lightblue;*/
     width: 50%;
     border: 1px solid grey;
+
 
     // 声明 class 前缀
     $class-prefix: col-;
@@ -28,6 +41,13 @@
     @for $n from 1 through 24 {
       &.#{$class-prefix}#{$n} {
         width: ($n / 24) * 100%;
+      }
+    }
+
+    $class-prefix: offset-;
+    @for $n from 1 through 24 {
+      &.#{$class-prefix}#{$n} {
+        margin-left: ($n / 24) * 100%;
       }
     }
   }
--- src/row.vue
+++ src/row.vue
@@ -1,8 +1,25 @@
 <template>
-  <div class="row">
+  <div class="row" :style="{ marginLeft: -gutter/2 + 'px', marginRight: -gutter/2 + 'px' }">
     <slot></slot>
   </div>
 </template>
+
+<script>
+  export default {
+    name: 'GearRow',
+    props: {
+      gutter: {
+        type: [Number, String]
+      }
+    },
+    mounted () {
+      // 通过 mounted 传递 row 的 gutter 值给 col
+      this.$children.forEach((vm) => {
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
 
 <style lang="scss" scoped>
   .row {
