8d93cce48a069d2e9cf043c40034e6da08e60a36
evenyao <invictus@vip.qq.com>
2018-11-11 13:43:55 +0800

    更新 toast 组件

--- src/toast.vue
+++ src/toast.vue
@@ -1,11 +1,20 @@
 <template>
-
+  <div class="toast"></div>
 </template>
 
 <script>
+  import Vue from 'vue'
 
+  Vue.prototype.$toast = function(){
+    console.log('我是 toast')
+  }
+  export default {
+    name: 'GearToast',
+  }
 </script>
 
 <style lang="scss" scoped>
+  .toast {
 
+  }
 </style>
