9144a923014e97f5f0818c45f7bc29157e73d94a
evenyao <invictus@vip.qq.com>
2018-11-12 11:56:32 +0800

    完善 toast 动画效果, 区分 top/middle/bottom 效果。

--- src/toast.vue
+++ src/toast.vue
@@ -100,9 +100,18 @@
   $font-size: 14px;
   $toast-min-height: 40px;
   $toast-bg: rgba(0,0,0,.75);
-  @keyframes fade-in {
+  $animation-duration: 0.5s;
+  @keyframes slide-up {
     0% { opacity: 0; transform: translateY(100%)}
     100% { opacity: 1; transform: translateY(0%)}
+  }
+  @keyframes slide-down {
+    0% { opacity: 0; transform: translateY(-100%)}
+    100% { opacity: 1; transform: translateY(0%)}
+  }
+  @keyframes fade-in {
+    0% { opacity: 0; }
+    100% { opacity: 1; }
   }
   .wrapper {
     position: fixed;
@@ -110,17 +119,27 @@
     transform: translateX(-50%);
     &.position-top {
       top: 0;
+      .toast {
+        border-top-left-radius: 0;
+        border-top-right-radius: 0;
+        animation: slide-down $animation-duration;
+      }
     }
     &.position-bottom {
       bottom: 0;
+      .toast {
+        border-bottom-left-radius: 0;
+        border-bottom-right-radius: 0;
+        animation: slide-up $animation-duration;
+      }
     }
     &.position-middle {
       top: 50%;
       transform: translateX(-50%) translateY(-50%);
+      animation: fade-in $animation-duration;
     }
   }
   .toast {
-    animation: fade-in .5s;
     font-size: $font-size; line-height: 1.8; min-height: $toast-min-height;
     display: flex; align-items: center;
     color: white;
