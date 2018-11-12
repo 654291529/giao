c2aeb426969dc1f9b57c402f9137267d114a7bad
evenyao <invictus@vip.qq.com>
2018-11-12 10:59:46 +0800

    实现 触发toast, toast 不会重复出现

--- src/plugin.js
+++ src/plugin.js
@@ -1,22 +1,34 @@
 /* 插件 让用户使用 */
 import  Toast from './toast'
 
+let currentToast
+
 export default {
   install (Vue, options) {
     Vue.prototype.$toast = function(message, toastOptions) {
-      var Constructor = Vue.extend(Toast)
-      let toast = new Constructor({
-        propsData: toastOptions
-      })
-
-      // 给 toast 传递默认插槽内容
-      toast.$slots.default = [message]
-
-      // mount
-      toast.$mount()
-
-      // 将元素放置到 body
-      document.body.appendChild(toast.$el)
+      if (currentToast) {
+        currentToast.close()
+      }
+      currentToast = createToast({ Vue, message, propsData: toastOptions }) //新建一个函数
     }
   }
+}
+
+
+
+/* helpers */
+function createToast({ Vue, message, propsData }) {
+  let Constructor = Vue.extend(Toast)
+  let toast = new Constructor({ propsData })
+
+  // 给 toast 传递默认插槽内容
+  toast.$slots.default = [message]
+
+  // mount
+  toast.$mount()
+
+  // 将元素放置到 body
+  document.body.appendChild(toast.$el)
+
+  return toast
 }
