e593bd02d7686749aa8d45490ae94d183b476600
evenyao <invictus@vip.qq.com>
2018-11-19 16:31:50 +0800

    解决组件上方如果有其他组件, 导致 popover 浮层内容定位错误的问题。

--- src/popover.vue
+++ src/popover.vue
@@ -23,7 +23,7 @@
             document.body.appendChild(this.$refs.contentWrapper)
             let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
             this.$refs.contentWrapper.style.left = left + 'px'
-            this.$refs.contentWrapper.style.top = top + 'px'
+            this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
 
 
             let eventHandler = () => {
