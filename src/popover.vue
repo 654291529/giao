aacb165fa6e1040b9c02cc01e19d593459517749
evenyao <invictus@vip.qq.com>
2018-11-19 11:05:12 +0800

    创建 popover 组件基本功能和基本样式

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="popover">
-    <slot name="content"></slot>
+  <div class="popover" @click="xxx">
+    <div class="content-wrapper" v-if="visible">
+      <slot name="content"></slot>
+    </div>
     <slot></slot>
   </div>
 </template>
@@ -8,6 +10,15 @@
 <script>
   export default {
     name: 'GearPopover',
+    data () {
+      return { visible: false }
+    },
+    methods: {
+      xxx() {
+        console.log(123)
+        this.visible = ! this.visible
+      }
+    }
   }
 </script>
 
@@ -15,5 +26,13 @@
   .popover {
     display: inline-block;
     vertical-align: top;
+    position: relative;
+    .content-wrapper {
+      position: absolute;
+      bottom: 100%;
+      left: 0;
+      border: 1px solid red;
+      box-shadow: 0 0 3px rgba(0, 0, 0, .5);
+    }
   }
 </style>
