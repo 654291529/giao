36572ab03043abdda5bed75b704c25b4f8ef40ff
evenyao <invictus@vip.qq.com>
2018-11-19 17:10:18 +0800

    重构优化代码。

--- src/popover.vue
+++ src/popover.vue
@@ -16,31 +16,36 @@
       return { visible: true }
     },
     methods: {
+      // 定位内容
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper)
+        let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      // 监听点击位置是否为内容区
+      listenToDocument () {
+        let eventHandler = (event) => {
+          if (!this.$refs.contentWrapper.contains(event.target)) {
+            this.visible = false
+            document.removeEventListener('click', eventHandler)
+          }
+        }
+        document.addEventListener('click', eventHandler)
+      },
+      // 显示在页面
+      onShow () {
+        this.$nextTick(() => {
+          this.positionContent()
+          this.listenToDocument()
+        })
+      },
       handleClick (event) {
-        console.log(event.target)
         if (this.$refs.triggerWrapper.contains(event.target)) {
-          console.log('下')
           this.visible = !this.visible
           if (this.visible === true) {
-            this.$nextTick(() => {
-              document.body.appendChild(this.$refs.contentWrapper)
-              let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
-              this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-              this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
-
-              let eventHandler = (e) => {
-                if (this.$refs.contentWrapper.contains(e.target)) {
-
-                } else {
-                  this.visible = false
-                  document.removeEventListener('click', eventHandler)
-                }
-              }
-              document.addEventListener('click', eventHandler)
-            })
+            this.onShow()
           }
-        } else {
-          console.log('上')
         }
       }
     },
