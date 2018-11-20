e060f064ee78c9b9bf8c0fb6690d3c3548c43d89
evenyao <invictus@vip.qq.com>
2018-11-20 17:05:14 +0800

    popover 组件添加 position 属性，添加 bottom 方位样式和逻辑

--- src/popover.vue
+++ src/popover.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="popover" @click="handleClick" ref="popover">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible" :class="{[`position-${position}`]:true}">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper" style="display: inline-block;">
@@ -15,13 +15,28 @@
     data () {
       return { visible: false }
     },
+    props: {
+      position: {
+        type: String,
+        default: 'top',
+        validator (value) {
+          return ['top', 'bottom', 'left', 'right'].indexOf(value) >= 0
+        }
+      }
+    },
     methods: {
       // 定位内容
       positionContent () {
         document.body.appendChild(this.$refs.contentWrapper)
         let { width, height, top, left } = this.$refs.triggerWrapper.getBoundingClientRect()
-        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+        if (this.position === 'top') {
+          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+        } else if (this.position === 'bottom') {
+          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+          this.$refs.contentWrapper.style.top = top + height + window.scrollY + 'px'
+        }
+
       },
       // 监听点击位置是否为内容区
       listenToDocument () {
@@ -79,12 +94,9 @@
     /*box-shadow: 0 0 3px rgba(0, 0, 0, .5);*/
     filter: drop-shadow(0 1px 1px rgba(0, 0, 0, .5));
     background: white;
-    transform: translateY(-100%);
     padding: .5em 1em;
-    margin-top: -10px;
     max-width: 20em;
     word-break: break-all;
-
     /* 气泡三角形 */
     &::before, &::after {
       content: '';
@@ -93,15 +105,36 @@
       width: 0;
       height: 0;
       position: absolute;
-      left: 10px;
     }
-    &::before {
-      border-top-color: black;
-      top: 100%;
+    &.position-top {
+      transform: translateY(-100%);
+      margin-top: -10px;
+      &::before, &::after {
+        left: 10px;
+      }
+      &::before {
+        border-top-color: black;
+        top: 100%;
+      }
+      &::after {
+        border-top-color: white;
+        top: calc(100% - 1px);
+      }
     }
-    &::after {
-      border-top-color: white;
-      top: calc(100% - 1px);
+
+    &.position-bottom {
+      margin-top: 10px;
+      &::before, &::after {
+        left: 10px;
+      }
+      &::before {
+        border-bottom-color: black;
+        bottom: 100%;
+      }
+      &::after {
+        border-bottom-color: white;
+        bottom: calc(100% - 1px);
+      }
     }
   }
 </style>
