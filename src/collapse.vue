1aa4c58d09514a79518da227ca57318ecd40b4d8
evenyao <invictus@vip.qq.com>
2018-11-29 15:22:07 +0800

    collapse 添加 selected, 限制只能选择一个

--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,19 @@
 </template>
 
 <script>
+  import Vue from 'vue'
   export default {
-    name: "GearCollapse"
+    name: "GearCollapse",
+    data () {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide () {
+      return {
+        eventBus: this.eventBus
+      }
+    }
   }
 </script>
 
