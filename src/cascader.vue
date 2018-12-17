d889cfe4228ddd1e5dc1e225f56b83fd3e9714bf
evenyao <invictus@vip.qq.com>
2018-12-17 16:59:00 +0800

    重写之前的 cascader 实现逻辑, 使用递归的方式渲染内容

--- src/cascader.vue
+++ src/cascader.vue
@@ -3,32 +3,16 @@
     <div class="trigger" @click="popoverVisible = !popoverVisible">
     </div>
     <div class="popover" v-if="popoverVisible">
-      <div class="level1">
-        <div class="label" v-for="item1 in source" style="border: 1px solid red;"
-        @click="level1Selected = item1">
-          {{item1.name}}
-        </div>
-      </div>
-      <div class="level2">
-        <div class="label" v-for="item2 in level2Items"
-        @click="level2Selected = item2">
-          {{item2.name}}
-        </div>
-      </div>
-      <div class="level3">
-        <div class="label" v-for="item3 in level3Items">
-          {{item3.name}}
-        </div>
-      </div>
+      <gear-cascader-items :items="source"></gear-cascader-items>
     </div>
   </div>
 </template>
 
 <script>
-  import CascaderItem from './cascader-item'
+  import CascaderItems from './cascader-items'
   export default {
     name: "GearCascader",
-    components: { 'gear-cascader-item': CascaderItem },
+    components: { 'gear-cascader-items': CascaderItems },
     props: {
       source: {
         type: Array
