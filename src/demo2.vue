12f49cac7b0e1091818bb36fbae82b4fa9dbe8a0
evenyao <invictus@vip.qq.com>
2018-12-26 11:54:20 +0800

    🔧 创建 slides 组件

---
+++ src/demo2.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <gear-slides>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </gear-slides>
+  </div>
+</template>
+
+<script>
+  import Slides from './slides'
+
+  export default {
+    name: 'demo2',
+    components: {
+      'gear-slides': Slides
+    },
+    data(){
+      return {
+
+      }
+    }
+  }
+</script>
+
+<style >
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+</style>
