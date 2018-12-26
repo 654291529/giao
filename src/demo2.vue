67d00a9e1b653d40fa378c73c6a30b2bcf2b6270
evenyao <invictus@vip.qq.com>
2018-12-26 16:23:34 +0800

    💡实现 slides 简单动画切换

--- src/demo2.vue
+++ src/demo2.vue
@@ -1,33 +1,58 @@
 <template>
   <div>
-    <gear-slides>
-      <div>1</div>
-      <div>2</div>
-      <div>3</div>
+    <gear-slides :selected="selected">
+      <gear-slides-item name="1">
+        <div class="box">1</div>
+      </gear-slides-item>
+      <gear-slides-item name="2">
+        <div class="box">2</div>
+      </gear-slides-item>
+      <gear-slides-item name="3">
+        <div class="box">3</div>
+      </gear-slides-item>
     </gear-slides>
   </div>
 </template>
 
 <script>
   import Slides from './slides'
+  import SlidesItem from './slides-item'
 
   export default {
     name: 'demo2',
     components: {
-      'gear-slides': Slides
+      'gear-slides': Slides,
+      'gear-slides-item': SlidesItem
     },
-    data(){
+    data() {
       return {
-
+        selected: '1'
       }
+    },
+    created() {
+      let n = 1
+      setInterval(() => {
+        if(n === 4) {
+          n = 1
+        }
+        this.selected = n.toString()
+        n++
+      },2000)
     }
   }
 </script>
 
-<style >
+<style>
   * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
+  }
+
+  .box {
+    width: 200px;
+    height: 150px;
+    background: #ddd;
+    border: 1px solid red;
   }
 </style>
--- src/slides.vue
+++ src/slides.vue
@@ -1,15 +1,46 @@
 <template>
-  <div>
-    <slot></slot>
+  <div class="gear-slides">
+    <div class="gear-sildes-window" ref="window">
+      <div class="gear-slides-wrapper">
+        <slot></slot>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-  export default  {
-
+  export default {
+    props: {
+      selected: {
+        type: String,
+      }
+    },
+    mounted() {
+      this.updateChildren()
+    },
+    updated () {
+      this.updateChildren()
+    },
+    methods: {
+      updateChildren () {
+        let first = this.$children[0]
+        let selected = this.selected || first.name
+        this.$children.forEach((vm) => {
+          vm.selected = selected
+        })
+      }
+    }
   }
 </script>
 
 <style lang="scss" scoped>
+  .gear-slides {
+    display: inline-block;
+    &-window {
 
+    }
+    &-wrapper {
+      position: relative;
+    }
+  }
 </style>
