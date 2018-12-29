2fe747bf2f25a25ca651a7d1ee634872e80b22e8
evenyao <invictus@vip.qq.com>
2018-12-29 16:34:48 +0800

    ✨NPM 包添加 dist/index.css

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import Vue from "vue";
+import Born from "./born.vue";
 import Demo from "./demo.vue";
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(Demo)
+  render: h => h(Born)
 }).$mount("#app");
 
