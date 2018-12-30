fa4df427f949488a5248a0e0ee5de9bdeb17f947
evenyao <invictus@vip.qq.com>
2018-12-30 22:04:54 +0800

    🔧 再次回退简洁版本的 index.js 入口文件

--- src/index.js
+++ src/index.js
@@ -1,67 +1,25 @@
-import Icon from './base/icon/icon'
-import Button from './base/button/button'
-import ButtonGroup from './base/button/button-group'
-import Input from './form/input/input'
-import Row from './layout/grid/row'
-import Col from './layout/grid/col'
-import Layout from './layout/layout/layout'
-import Header from './layout/layout/header'
-import Sider from './layout/layout/sider'
-import Content from './layout/layout/content'
-import Footer from './layout/layout/footer'
-import Toast from './notify/toast/toast'
-import plugin from './plugins/plugin'
-import Tabs from './datashow/tabs/tabs'
-import TabsItem from './datashow/tabs/tabs-item'
-import TabsNav from './datashow/tabs/tabs-nav'
-import TabsPane from './datashow/tabs/tabs-pane'
-import TabsContent from './datashow/tabs/tabs-content'
-import Popover from './notify/popover/popover'
-import Cascader from './form/cascader/cascader'
-import CascaderItems from './form/cascader/cascader-items'
-import Collapse from './datashow/collapse/collapse'
-import CollapseItem from './datashow/collapse/collapse-item'
-import Springs from './action/springs/springs'
-import Slides from './datashow/slides/slides'
-
-
-const components = [
-  Icon,
-  Button,
-  ButtonGroup,
-  Input,
-  Row,
-  Col,
-  Layout,
-  Header,
-  Sider,
-  Content,
-  Footer,
-  Toast,
-  plugin,
-  Tabs,
-  TabsItem,
-  TabsNav,
-  TabsPane,
-  TabsContent,
-  Popover,
-  Cascader,
-  CascaderItems,
-  Collapse,
-  CollapseItem,
-  Springs,
-  Slides
-]
-
-
-const install = Vue => {
-  components.forEach(component => {
-    component.install(Vue)
-  })
-  Vue.use(Message)
-  Vue.use(Loading)
-}
-
-
-export { Message, Loading, Formator }
-export default { install, Loading, Message, Formator }
+export { default as Icon } from './base/icon/icon'
+export { default as Button } from './base/button/button'
+export { default as ButtonGroup } from './base/button/button-group'
+export { default as Input } from './form/input/input'
+export { default as Row } from './layout/grid/row'
+export { default as Col } from './layout/grid/col'
+export { default as Layout } from './layout/layout/layout'
+export { default as Header } from './layout/layout/header'
+export { default as Sider } from './layout/layout/sider'
+export { default as Content } from './layout/layout/content'
+export { default as Footer } from './layout/layout/footer'
+export { default as Toast } from './notify/toast/toast'
+export { default as plugin } from './plugins/plugin'
+export { default as Tabs } from './datashow/tabs/tabs'
+export { default as TabsItem } from './datashow/tabs/tabs-item'
+export { default as TabsNav } from './datashow/tabs/tabs-nav'
+export { default as TabsPane } from './datashow/tabs/tabs-pane'
+export { default as TabsContent } from './datashow/tabs/tabs-content'
+export { default as Popover } from './notify/popover/popover'
+export { default as Cascader } from './form/cascader/cascader'
+export { default as CascaderItems } from './form/cascader/cascader-items'
+export { default as Collapse } from './datashow/collapse/collapse'
+export { default as CollapseItem } from './datashow/collapse/collapse-item'
+export { default as Springs } from './action/springs/springs'
+export { default as Slides } from './datashow/slides/slides'
